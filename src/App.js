591fd0ca8459fa89561bc44fc10a8a8e278e263b
himanshudamani <himanshuvdamani@gmail.com>
2019-05-27 23:52:04 +0530

    Components part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 function App() {
